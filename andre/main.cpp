d6f69b0ad72fbc8f706068fffeec8b9e9ed618d8
wvbcity <wvbcity@e89925b4-5ba2-4528-b0fa-2e8be50954d4>
2010-12-10 15:30:14 +0000

    neue Version mit - funktionierendem Barcode, auch mit höheren Geschwindigkeiten (bis 80 getestet) - funktionierender Abstandsmessung (teilweise xP)

---
+++ andre/main.cpp
@@ -0,0 +1,271 @@
+#include "robot.h"
+
+namespace common { // Allgemeines
+	// Zeit die auf der Codefarbe verbracht werden muss, damit Code erkannt wird
+	// Wird automatisch an die Geschwindigkeit angepasst
+	static const unsigned long MAX_TIME = 50; // 50 ms 50 speed --> 25 ms 100 speed?
+}
+
+namespace speed { // Geschwindigkeitsmodul --> TODO
+	static const unsigned int DEFAULT_LEFT = 80;
+	static const unsigned int DEFAULT_RIGHT = 80;
+	int left = DEFAULT_LEFT; // Geschwindigkeit des linken Rades
+	int right = DEFAULT_RIGHT; // Geschwindigkeit des rechten Rades
+
+	unsigned long MAX_TIME = 500;
+	unsigned long time = 0;
+
+	void runSpeed(Robot *robot, unsigned long delay);
+}
+
+namespace code { // Codemodul
+	// Konstanten für die Position des Codemoduls
+	static const unsigned int NOCODE = 1; // Kein Code
+	static const unsigned int BEGIN = 2; // Codeanfangsstrich
+	static const unsigned int CODE = 3; // Code
+	static const unsigned int END = 4; // Codeendstrich
+
+	unsigned long time = 0; // Zeit seit nur noch Codefarbe erkannt wurde
+
+	unsigned int status = NOCODE; // Status des Codemoduls
+
+	int code = -1; // der eingelesene Code, -1 heißt dass kein Code gelesen wurde
+	unsigned int nextTime = 0; // Zeit bis zum nächsten mal Farbwert für Code einlesen
+	unsigned int bitTime = 0; // Zeit die das erste Bit des Codes in etwa zum Überfahren benötigt
+	unsigned int values[40]; // Alle eingelesenen Farbewerte
+	unsigned int index = 0; // Index des nächste Farbwertes
+	unsigned int average[4];
+
+	void runCode(Robot *robot, unsigned long delta); // Zum Ausführen des Codemodul
+}
+
+namespace line { // Linienmodul
+	// Konstanten für den Status des Linienmoduls
+	static const unsigned int FORWARD = 0; // Vorwärts
+	static const unsigned int LEFT = 1; // Links Teil 1
+	static const unsigned int LEFT2 = 2; // Links Teil 2
+	static const unsigned int RIGHT = 3; // Rechts Teil 1
+	static const unsigned int RIGHT2 = 4; // Rechts Teil 2
+
+	unsigned int status = FORWARD; // Aktueller Status des Linienmoduls
+
+	void runLine(Robot *robot, unsigned long delta); // Zum Ausführen des Linienmoduls
+}
+
+int main() {  
+	unsigned long time = 0; // Zeitpunkt am Anfang eines Schleifendurchlaufs
+	unsigned long delta = 0; // Zeit die ein Schleifendurchlauf benötigt
+	const unsigned long delay = 10; // Zeit die pro Schleifendurchlauf gewartet wird
+	
+	OrangutanTime::reset();
+
+	Robot robot;
+
+	robot.initWait("Ready!");
+
+	while(true) { // Hauptschleife
+		time = robot.ms();
+
+		robot.clear();
+		robot.update();
+
+		// Linien- und Codemodul"
+
+		//*
+		speed::runSpeed(&robot, delay);
+		line::runLine(&robot, delay);
+		code::runCode(&robot, delay);
+//
+		//*/
+		
+		// Testausgabe zum Farbwerte anzeigen
+
+		/*
+		unsigned int *sensors = robot.getLineSensorsClean();
+		robot.print(sensors[0]);
+		robot.print(" ");
+		robot.print(sensors[1]);
+		robot.print(" ");
+		robot.print(sensors[2]);
+		robot.move(0,1);
+
+		unsigned int *sensors2 = robot.getLineSensorsCalibrate();
+		robot.print(sensors2[0]);
+		robot.print(" ");
+		robot.print(sensors2[1]);
+		//*/
+
+		robot.delay(delay);
+
+		delta = robot.ms() - time; // Berechnet die Zeit die ein Schleifendurchlauf benötigt hat
+	}
+	return 0;
+}
+
+namespace speed {
+	void runSpeed(Robot *robot, unsigned long delta) {
+		bool *sensors = robot->getDistanceSensors();
+		unsigned long maxTime = (unsigned long)(((float)speed::MAX_TIME/robot->getSpeedAverage())*speed::MAX_TIME);
+		if(sensors[0]) {
+			if(time+delta <= maxTime) {
+				time += delta;
+			} else {
+				time = maxTime;
+			}
+		} else {
+			if(time>=delta) {
+				time -= delta;
+			} else {
+				time = 0;
+			}
+		}
+		float factor = 1-(time/(float)maxTime);
+		speed::left = (int)(speed::DEFAULT_LEFT*factor);
+		speed::right = (int)(speed::DEFAULT_RIGHT*factor);
+	}
+}
+
+namespace code { // Codemodul
+	void runCode(Robot *robot, unsigned long delta) {
+		unsigned int *sensors = robot->getLineSensorsClean();
+		
+		unsigned long maxTime = (unsigned long)(((float)common::MAX_TIME/robot->getSpeedAverage())*common::MAX_TIME);
+
+		if(line::status == line::FORWARD) {
+			if(status == NOCODE) {
+				robot->print("N");
+				if((sensors[0] & Robot::CODE)) {
+					time += delta;
+					bitTime += delta;
+					if(time >= maxTime) {
+						status = BEGIN;
+						time = 0;
+					}
+				} else {
+					bitTime = 0;
+					time = 0;
+				}
+			} else if(status == BEGIN) {
+				robot->print("B");
+				bitTime += delta;
+				if(!(sensors[0] & Robot::CODE)) {
+					index = 0;
+					nextTime = 0;
+					status = CODE;
+				}
+			} else if(status == CODE) {
+				robot->print("C");
+				nextTime += delta;
+				unsigned int *raw = robot->getLineSensorsCalibrate();
+				
+				// code lesen
+				while(nextTime >= bitTime/10 && index < 40) { // falls wieder Zeit ist
+					nextTime -= (bitTime/10); // delta
+					values[index++] = raw[0];
+				}
+
+				// code ende?
+				if((sensors[0] & Robot::CODE)) {
+					time += delta;
+					if(time >= maxTime) {
+						status = END;
+						time = 0;
+						// code berechnen
+						code = 0;
+						for(unsigned int i = 0; i < 4; i++) {
+							unsigned long sum = 0;
+							for(unsigned int j = 0; j < (int)(index/4.0); j++) {
+								sum += values[i*10+j];
+							}
+							unsigned int bit = (unsigned int)(sum/(index/4.0));
+							average[i] = bit;
+							if(bit > 400) {
+								code |= (1<<i);
+							}
+						}
+					}
+				} else {
+					time = 0;
+				}	
+			} else if(status == END) {
+				robot->print("E");
+				if(!(sensors[0] & Robot::CODE)) {
+					status = NOCODE;
+					// Ergebnisausgabe des Codes
+					/*
+					robot->setSpeed(0,0);
+					robot->update();
+					robot->delay(1000);
+					robot->clear();
+					robot->print("i=");
+					robot->print(index);
+					robot->delay(1000);
+					for(int i = 0; i < 4; i++) {
+						robot->clear();
+						robot->print("avg=");
+						robot->print(average[i]);
+						robot->delay(1000);
+					}
+					robot->delay(1000);
+					for(unsigned int i = 0; i < index; i++) {
+						robot->clear();
+						robot->print(values[i]);
+						robot->delay(250);
+					}//*/
+					//
+				}
+			}
+		} else {
+			robot->print("N");
+		}
+
+		robot->print(" ");
+		robot->print(bitTime);
+		robot->print(" ");
+		robot->print(code);
+		robot->move(0,1);
+	}
+}
+
+namespace line {
+	void runLine(Robot *robot, unsigned long delta) {
+		unsigned int *sensors = robot->getLineSensorsClean();
+		if(status == FORWARD) {
+			if(sensors[2] & Robot::LINE) {
+				robot->setSpeed(speed::left, speed::right);	
+			} else if(sensors[1] & Robot::LINE) {
+				robot->setSpeed((int)(speed::left*0.83), (int)(speed::right*1.16));
+			} else if(sensors[3] & Robot::LINE) {
+				robot->setSpeed((int)(speed::left*1.16), (int)(speed::right*0.83));
+			}
+
+			if(code::status == code::NOCODE) {
+				if((sensors[0] & Robot::LINE)) {
+					status = LEFT;
+				} else if((sensors[4] & Robot::LINE)) {
+					status = RIGHT;
+				}
+			}
+		} else if(status == LEFT) {
+			robot->setSpeed(speed::left, speed::right);
+			if(sensors[2] & Robot::BACK) {
+				status = LEFT2;
+			}		
+		} else if(status == LEFT2) {
+			robot->setSpeed(speed::left/-2, speed::right);
+			if(sensors[2] & Robot::LINE) {
+				status = FORWARD;
+			}
+		} else if(status == RIGHT) {
+			robot->setSpeed(speed::left, speed::right);
+			if(sensors[2] & Robot::BACK) {
+				status = RIGHT2;
+			}	
+		} else if(status == RIGHT2) {	
+			robot->setSpeed(speed::left, speed::right/-2);
+			if(sensors[2] & Robot::LINE) {
+				status = FORWARD;
+			}
+		}
+	}
+}
---
+++ andre/robot.cpp
@@ -0,0 +1,166 @@
+#include <pololu/orangutan>
+#include <pololu/Pololu3pi/Pololu3pi.h>
+
+#include "robot.h"
+
+
+Robot::Robot() {
+    pol.init(2000); //init
+	setDistance(450);
+	initWait("  cali  ");
+	lineSensorsCalibrate();
+}
+
+void Robot::update() {
+	OrangutanMotors::setSpeeds(speedl, speedr); //update speed
+	pol.readLineSensors(sensor_line_raw, IR_EMITTERS_ON); //update line sensors raw
+	pol.readLineSensorsCalibrated(sensor_line_calibrate, IR_EMITTERS_ON); //update line sensors calibrate
+	for(int i=0;i<5;i++) {
+		if(sensor_line_calibrate[i]<CALI_CODE) {
+			sensor_line_clean[i] = BACK;
+		} else if(sensor_line_calibrate[i]<CALI_LINE) {
+			sensor_line_clean[i] = CODE;
+		} else {
+			sensor_line_clean[i] = LINE;
+		}
+	}
+	sensor_distance[0] = OrangutanAnalog::read(7)<getDistance();
+	sensor_distance[1] = OrangutanAnalog::read(6)<getDistance();
+}
+
+void Robot::initWait() {
+	initWait("", "Press B!", BUTTON_B);
+}
+
+void Robot::initWait(const char *line1) {
+	initWait(line1, "Press B!", BUTTON_B);
+}
+
+void Robot::initWait(const char *line1, const char *line2, unsigned char buttons) {
+	clear();
+	print(line1, line2);
+	setSpeed(0, 0);
+	update();
+	waitForPress(buttons);
+}
+
+//speed set
+void Robot::setSpeedLeft(int left) {
+	if(left>-256 && left<256) {
+		this->speedl = left;
+	}
+}
+void Robot::setSpeedRight(int right) {
+	if(right>-256 && right<256) {
+		this->speedr = right;
+	}
+}
+void Robot::setSpeed(int left, int right) {
+	this->setSpeedLeft(left);
+	this->setSpeedRight(right);
+}
+
+//speed get
+int Robot::getSpeedLeft() {
+	return speedl;
+}
+int Robot::getSpeedRight() {
+	return speedr;
+}
+int Robot::getSpeedAverage() {
+	return (int)(speedr+speedl)/2;
+}
+
+unsigned int Robot::getDirection() {
+	if(speedl == speedr) {
+		return STRAIGHT;
+	} else if(speedl < speedr) {
+		return LEFT;
+	} else {
+		return RIGHT;
+	}
+}
+
+//sensor line
+unsigned int *Robot::getLineSensorsRaw() {
+	return sensor_line_raw;
+}
+unsigned int *Robot::getLineSensorsClean() {
+	return sensor_line_clean;
+}
+unsigned int *Robot::getLineSensorsCalibrate() {
+	return sensor_line_calibrate;
+}
+
+//sensor line cali
+void Robot::lineSensorsCalibrate() {
+	clear();
+	print("  cali  ", "progress");
+	for(int counter=0;counter<80;counter++) {
+        if(counter < 20 || counter >= 60) {
+            OrangutanMotors::setSpeeds(30, -30);
+        } else {
+       	    OrangutanMotors::setSpeeds(-30, 30);
+		}
+		pol.calibrateLineSensors();
+        OrangutanTime::delayMilliseconds(20);
+    }
+	OrangutanMotors::setSpeeds(0, 0);
+}
+
+//sensor distance
+void Robot::setDistance(unsigned int dist) {
+	this->dist = dist;
+}
+unsigned int Robot::getDistance() {
+	return dist;
+}
+bool *Robot::getDistanceSensors() {
+	return sensor_distance;
+}
+
+//lcd
+void Robot::print(long value) {
+	OrangutanLCD::print(value);
+}
+void Robot::print(const char *str) {
+	OrangutanLCD::print(str);
+}
+void Robot::print(const char *line1, const char *line2) {
+	print(line1);
+	move(0, 1);
+	print(line2);
+}
+void Robot::clear() {
+	OrangutanLCD::clear();
+}
+void Robot::move(unsigned char x, unsigned char y) {
+	OrangutanLCD::gotoXY(x, y);
+}
+
+//time
+void Robot::delay(unsigned int milliseconds) {
+	OrangutanTime::delayMilliseconds(milliseconds);
+}
+unsigned long Robot::ms() {
+	return OrangutanTime::ms();
+}
+
+//buttons
+unsigned char Robot::waitForPress(unsigned char buttons) {
+	return OrangutanPushbuttons::waitForPress(buttons);
+}
+unsigned char Robot::waitForRelease(unsigned char buttons) {
+	return OrangutanPushbuttons::waitForRelease(buttons);
+}
+unsigned char Robot::waitForButton(unsigned char buttons) {
+	return OrangutanPushbuttons::waitForButton(buttons);
+}
+unsigned char Robot::isPressed(unsigned char buttons) {
+	return OrangutanPushbuttons::isPressed(buttons);
+}
+
+//buzzer
+void Robot::play(const char *sequence) {
+	OrangutanBuzzer::play(sequence);
+}
